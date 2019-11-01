35060a2ab5b77fb76ad9f9a4037b823884764980
Luz <ulsa@krlos.local>
2019-11-01 13:53:39 -0700

    Table view, falta llenar con informacion de wordpress

---
+++ eventos/CeldaEventoController.swift
@@ -0,0 +1,18 @@
+//
+//  CeldaEventoController.swift
+//  eventos
+//
+//  Created by Alumno on 11/1/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaEventoController : UITableViewCell {
+    
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var lblFecha: UILabel!
+    @IBOutlet weak var imgFlyer: UIImageView!
+    
+}
